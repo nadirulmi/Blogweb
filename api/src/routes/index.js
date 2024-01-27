7a064e8e53eabcd261c6281fa4b1808e4b997e5b
ulminadir <ulminadir99@gmail.com>
2024-01-27 12:36:49 -0300

    endpoints para poder utilizar en front

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
+const { getPost } = require('../controllers/getPost');
+const {getDetail} = require("../controllers/getDetail");
+const { getCommentById } = require('../controllers/getCommentById');
 
 const router = Router();
 
-router.get('/post', ()=>{
-    console.log("rutas funcionando")
-});
+router.get('/post', getPost);
+router.get('/detail/:id', getDetail);
+router.get('/comment/:id', getCommentById);
+
 
 module.exports = router;
