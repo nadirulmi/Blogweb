6a55cab6e9b79582c78388db25c9b63d7170fb26
ulminadir <ulminadir99@gmail.com>
2024-01-27 14:14:44 -0300

    componente pie de pagina

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-black p-6 mt-6">
+      <p className="text-center" style={{ color: "#F68E1E" }}>
+        Copyright © Nadir Yamila Ulmi. Todos los derechos reservados.
+      </p>
+    </footer>
+  );
+};
