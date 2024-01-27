54934606dba1234d7f96bf66f97b0ece573c02b5
ulminadir <ulminadir99@gmail.com>
2024-01-27 13:10:56 -0300

    rutas get de users y tags

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,12 +2,15 @@ const { Router } = require('express');
 const { getPost } = require('../controllers/getPost');
 const {getDetail} = require("../controllers/getDetail");
 const { getCommentById } = require('../controllers/getCommentById');
+const { getTags } = require('../controllers/getTags');
+const {getUsers} = require("../controllers/getUsers")
 
 const router = Router();
 
 router.get('/post', getPost);
 router.get('/detail/:id', getDetail);
 router.get('/comment/:id', getCommentById);
-
+router.get('/tags',getTags)
+router.get('/users',getUsers)
 
 module.exports = router;
